096318b179f875d3055adb03a1389c656b46f21f
Ennio <ennio.strohauer@gmail.com>
2023-06-20 20:01:12 +0200

    Adjust method for image sharpening only. Implement patch processing

--- Test.m
+++ Test.m
@@ -6,24 +6,29 @@ iter = 4;
 p = 0.8;
 eps = 0.0001; 
 
-Img = im2double((imread('flower.png')));
-Smoothed = ILS_LNorm(Img, lambda, p, eps, iter);
-% Smoothed = ILS_LNorm_GPU(Img, lambda, p, eps, iter);
+% Specify the directory path where the images are located
+imageDir = '/Users/ennios/Desktop/MML_Medical_Machine_Learning/endonerf_sample_datasets/cutting_tissues_twice/images_right/';
+targetDir = '/Users/ennios/Desktop/MML_Medical_Machine_Learning/Images_ILS_Sharpened/images_right/'
 
-Diff = Img - Smoothed;
-ImgE = Img + 3 * Diff;
-
-figure; imshow(Smoothed)
-figure; imshow(ImgE)
-
-%%
-Img = im2double(imread('clip_art.jpg'));
-lambda = 30;
-gamma = 10/255;
-iter = 10;
-
-Smoothed = ILS_Welsch(Img, lambda, gamma, iter);
-% Smoothed = ILS_Welsch_GPU(Img, lambda, gamma, iter);
-
-figure; imshow(Img)
-figure; imshow(Smoothed)
+% Iterate over the images and apply the sharpening code
+for i = 0:155
+    % Generate the file name for the current image
+    filename = sprintf('%s%06d.png', imageDir, i);
+    
+    % Read the image
+    Img = im2double(imread(filename));
+    
+    % Apply the sharpening code (ILS_LNorm function)
+    Smoothed = ILS_LNorm(Img, lambda, p, eps, iter);
+    Diff = Img - Smoothed;
+    ImgE = Img + Diff;
+    
+    % Generate the file name for the sharpened image
+    newFilename = sprintf('%s%06d.png', targetDir, i);
+    
+    % Save the sharpened image
+    imwrite(ImgE, newFilename);
+    
+    % Display a message for each image processed
+    disp(['Processed image ' num2str(i) ' out of 155']);
+end
