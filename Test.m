8aadf939f9e4454ed53664a2aa16eb2553043c59
Ennio <ennio.strohauer@gmail.com>
2023-07-28 11:15:19 +0200

    "add batch processing, add method for sharpening chroma only"

--- Test.m
+++ Test.m
@@ -7,13 +7,13 @@ p = 0.8;
 eps = 0.0001; 
 
 % Specify the directory path where the images are located
-imageDir = '/Users/ennios/Desktop/MML_Medical_Machine_Learning/endonerf_sample_datasets/cutting_tissues_twice/images_right/';
-targetDir = '/Users/ennios/Desktop/MML_Medical_Machine_Learning/Images_ILS_Sharpened/images_right/'
+imageDir = '/Users/ennios/Desktop/MML_Medical_Machine_Learning/endonerf_sample_datasets/ctt_depth_sharpened/depth_StereoNet_plain/';
+targetDir = '/Users/ennios/Desktop/MML_Medical_Machine_Learning/endonerf_sample_datasets/ctt_depth_sharpened/sharpened_depth/ILS(0.5)/StereoNet_plain/';
 
 % Iterate over the images and apply the sharpening code
 for i = 0:155
     % Generate the file name for the current image
-    filename = sprintf('%s%06d.png', imageDir, i);
+    filename = sprintf('%sframe-%06d.depth.png', imageDir, i);
     
     % Read the image
     Img = im2double(imread(filename));
@@ -21,10 +21,10 @@ for i = 0:155
     % Apply the sharpening code (ILS_LNorm function)
     Smoothed = ILS_LNorm(Img, lambda, p, eps, iter);
     Diff = Img - Smoothed;
-    ImgE = Img + Diff;
+    ImgE = Img + 0.5 * Diff;
     
     % Generate the file name for the sharpened image
-    newFilename = sprintf('%s%06d.png', targetDir, i);
+    newFilename = sprintf('%sframe-%06d.depth.png', targetDir, i);
     
     % Save the sharpened image
     imwrite(ImgE, newFilename);
